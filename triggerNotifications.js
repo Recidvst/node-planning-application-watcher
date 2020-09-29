da93fcae3d97361b224bb12655e4e919e5248e04
recidvst <cdsnowden@gmail.com>
2020-09-29 16:38:51 +0100

    remove twilio SMS functionality as trial over, just use email

--- triggerNotifications.js
+++ triggerNotifications.js
@@ -1,8 +1,6 @@
 // env vars
-const NOTIFYBYTEXT = (process.env.NOTIFYBYTEXT === 'true') || false;
 const NOTIFYBYEMAIL = (process.env.NOTIFYBYEMAIL === 'true') || false;
 // actions
-const twilio = require('./twilio');
 const mailer = require('./mailer');
 // require log function
 const logToFile = require('./createFile.js');
@@ -14,11 +12,6 @@ const sendNotifications = async (date) => {
     // send mail
     if (NOTIFYBYEMAIL && mailer && typeof mailer === 'object') {
       mailer.sendMail();
-    }
-
-    // twilio messaging integration
-    if (NOTIFYBYTEXT && twilio && typeof twilio === 'object') {
-      twilio.twilioSendSMS();
     }
 
   }
